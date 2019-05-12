ef9b71d96daca2c2fa7c462a7f14c40bb7b6b634
Ross Whippo <rosspitality@gmail.com>
2019-05-12 14:04:43 -0300

    Created species list and added visualizations to collections data

--- Data/Inverts/Invert_Data.R
+++ Data/Inverts/Invert_Data.R
@@ -47,6 +47,8 @@
 library(tidyverse)
 library(worrms)
 library(reshape2)
+library(ggplot2)
+library(viridis)
 
 ###################################################################################
 # READ IN AND PREPARE DATA                                                        #
@@ -62,6 +64,15 @@ str(raw_inverts_0)
 raw_inverts_1 <- raw_inverts_0 %>%
   filter(SiteID != "NA")
 
+# recode ice cover cats
+
+raw_inverts_1$IceCoverCat <- raw_inverts_1$IceCoverCat %>%
+  recode("<=60%" = "0.6",
+         "<=80%" = "0.8",
+         "<=90%" = "0.9",
+         "50%" = "0.5",
+         "80%" = "0.8")
+raw_inverts_1$IceCoverCat <- as.numeric(as.character(raw_inverts_1$IceCoverCat))
 
 ###################################################################################
 # SPECIES NAMES & LIST                                                            #
@@ -94,26 +105,63 @@ incorrect_spnames <- full_join(species_notfound, failed_species)
 
 # fix incorrect species names
 raw_inverts_2 <- raw_inverts_1
-raw_inverts_2$genusSpecies %>%
+raw_inverts_2$genusSpecies <- raw_inverts_2$genusSpecies %>%
   recode("Neosmilaster georginus" = "Neosmilaster georgianus", 
          "Sterechinus neumeyeri" = "Sterechinus neumayeri",
          "Isotaelia antarctica" = "Isotealia antarctica",
          "Heterocucumis cucumaris" = "Heterocucumis steineni",
-         "Porania antarctica glabra" = "Glabraster antarctica")
+         "Porania antarctica glabra" = "Glabraster antarctica",
+         "Alcyonium antarctucum" = "Alcyonium antarcticum",
+         "Metalaptamphopus pectinatus" = "Metaleptamphopus pectinatus",
+         "Labidaster annulatus" = "Labidiaster annulatus",
+         "Promachocrinus kerguelenensis" = "Promachocrinus kerguelensis",
+         "Phorbus aerolatus" = "Phorbas areolatus")
 
 
 
 ####################### make species list
 
-species_list <-raw_inverts_1 %>% 
+species_list <-raw_inverts_2 %>% 
   group_by(genusSpecies) %>% 
   tally()
 
 write_csv(species_list, "B236_invert-species-list.csv")
 
 
+###################################################################################
+# COLLECTION VISUALIZATIONS                                                       #
+###################################################################################
 
+raw_inverts_f1 <- raw_inverts_2
+raw_inverts_f1$count <- 1
+# per individual
+raw_inverts_f2 <- raw_inverts_f1 %>%
+  group_by(IceCoverCat) %>%
+  summarise(sum(count))
+# rename column
+names(raw_inverts_f2)[names(raw_inverts_f2)=="sum(count)"] <- "totalOrganisms"
+# per species
+raw_inverts_f3 <- raw_inverts_f1 %>%
+  group_by(IceCoverCat, genusSpecies) %>% 
+  summarise(sum(count))
+# rename column
+names(raw_inverts_f3)[names(raw_inverts_f3)=="sum(count)"] <- "totalSpecies"
+raw_inverts_f3$totalSpecies <- 1
+
+
+# total number of organisms collected for biomarkers per ice cover
+ggplot(data = raw_inverts_f2, aes(x = IceCoverCat, y = totalOrganisms)) +
+  geom_col() +
+  theme_classic()
+
+# total number of species collected for biomarkers per ice cover
+ggplot(data = raw_inverts_f3, aes(x = IceCoverCat, y = totalSpecies)) +
+  geom_col() +
+  theme_classic()
+
+?viridis
 ############### SUBSECTION HERE
+str(raw_inverts_2)
 
 #####
 #<<<<<<<<<<<<<<<<<<<<<<<<<<END OF SCRIPT>>>>>>>>>>>>>>>>>>>>>>>>#
