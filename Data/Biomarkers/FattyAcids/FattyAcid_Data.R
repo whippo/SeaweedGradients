8b1aaa575d29f313bc154f8ca34cdb35cb210714
Ross Whippo <rosspitality@gmail.com>
2019-05-13 12:15:55 -0300

    Added more data, updated paths, added site level visualizations

--- Data/Biomarkers/FattyAcids/FattyAcid_Data.R
+++ Data/Biomarkers/FattyAcids/FattyAcid_Data.R
@@ -1,10 +1,10 @@
 ###################################################################################
 #                                                                                ##
-# B-236 Invertebrate Collections Data                                            ##
+# B-236 Fatty Acid Collections Data                                              ##
 # Data are current as of 2019-05-12                                              ##
 # Data source: B-236 Seaweed Gradients Cruise                                    ##
 # R code prepared by Ross Whippo                                                 ##
-# Last updated 2019-05-12                                                        ##
+# Last updated 2019-05-13                                                        ##
 #                                                                                ##
 ###################################################################################
 
@@ -16,7 +16,7 @@
 
 
 # Required Files (check that script is loading latest version):
-# B-236_Invert_Collections.csv
+# B-236_Fatty-Acid_Collections.csv
 
 # Associated Scripts:
 # none
@@ -39,6 +39,7 @@
 
 # 2019-05-12 Script created and added to git repository:
 # https://github.com/whippo/SeaweedGradients.git
+# 2019-05-13 Changed name of source files
 
 ###################################################################################
 # LOAD PACKAGES                                                                   #
@@ -54,15 +55,15 @@ library(viridis)
 # READ IN AND PREPARE DATA                                                        #
 ###################################################################################
 
-setwd("~/Git/SeaweedGradients/Data/Inverts")
+setwd("~/Git/SeaweedGradients/Data/Biomarkers/FattyAcids")
 
-raw_inverts_0 <- read.csv("B-236_Invert_Collections.csv")
+raw_inverts_0 <- read.csv("B-236_Fatty-Acid_Collections.csv")
 
 str(raw_inverts_0)
 
 # remove empty rows
 raw_inverts_1 <- raw_inverts_0 %>%
-  filter(SiteID != "NA")
+  filter(SiteID != "")
 
 # recode ice cover cats
 
@@ -71,7 +72,8 @@ raw_inverts_1$IceCoverCat <- raw_inverts_1$IceCoverCat %>%
          "<=80%" = "0.8",
          "<=90%" = "0.9",
          "50%" = "0.5",
-         "80%" = "0.8")
+         "80%" = "0.8",
+         "<=40%" = "0.4")
 raw_inverts_1$IceCoverCat <- as.numeric(as.character(raw_inverts_1$IceCoverCat))
 
 ###################################################################################
@@ -109,6 +111,7 @@ raw_inverts_2$genusSpecies <- raw_inverts_2$genusSpecies %>%
   recode("Neosmilaster georginus" = "Neosmilaster georgianus", 
          "Sterechinus neumeyeri" = "Sterechinus neumayeri",
          "Isotaelia antarctica" = "Isotealia antarctica",
+         "Isotealia antactica" = "Isotealia antarctica",
          "Heterocucumis cucumaris" = "Heterocucumis steineni",
          "Porania antarctica glabra" = "Glabraster antarctica",
          "Alcyonium antarctucum" = "Alcyonium antarcticum",
@@ -118,7 +121,12 @@ raw_inverts_2$genusSpecies <- raw_inverts_2$genusSpecies %>%
          "Phorbus aerolatus" = "Phorbas areolatus",
          "Metalaptamphopus pectatus" = "Metaleptamphopus pectinatus",
          "Bovalia gigantea" = "Bovallia gigantea",
-         "Austrodoris vergulensis" = "Austrodoris kerguelenensis")
+         "Austrodoris vergulensis" = "Doris kerguelenensis",
+         "Austrodoris kerguelenensis" = "Doris kerguelenensis",
+         "Pontogeneilla brevicornis" = "Prostebbingia brevicornis",
+         "Panaceradocus miersi" = "Paraceradocus miersi",
+         "Polynoidae so" = "Polynoidae sp."
+         )
 
 
 
@@ -128,41 +136,45 @@ species_list <-raw_inverts_2 %>%
   group_by(genusSpecies) %>% 
   tally()
 
-write_csv(species_list, "B236_invert-species-list.csv")
+write_csv(species_list, "B236_fattyacid-species-list.csv")
 
 ####################### species found at all sites
 
 Site1 <- raw_inverts_2 %>%
   group_by(SiteID, genusSpecies) %>%
-  filter(SiteID == 1)
+  filter(SiteID == "01")
 Site2 <- raw_inverts_2 %>%
   group_by(SiteID, genusSpecies) %>%
-  filter(SiteID == 2)
+  filter(SiteID == "02")
 Site3 <- raw_inverts_2 %>%
   group_by(SiteID, genusSpecies) %>%
-  filter(SiteID == 3)
+  filter(SiteID == "03")
 Site4 <- raw_inverts_2 %>%
   group_by(SiteID, genusSpecies) %>%
-  filter(SiteID == 4)
+  filter(SiteID == "04")
 Site5 <- raw_inverts_2 %>%
   group_by(SiteID, genusSpecies) %>%
-  filter(SiteID == 5)
+  filter(SiteID == "05")
 Site6 <- raw_inverts_2 %>%
   group_by(SiteID, genusSpecies) %>%
-  filter(SiteID == 6)
+  filter(SiteID == "06")
 Site7 <- raw_inverts_2 %>%
   group_by(SiteID, genusSpecies) %>%
-  filter(SiteID == 7)
+  filter(SiteID == "07")
 Site8 <- raw_inverts_2 %>%
   group_by(SiteID, genusSpecies) %>%
-  filter(SiteID == 8)
+  filter(SiteID == "08")
 Site9 <- raw_inverts_2 %>%
   group_by(SiteID, genusSpecies) %>%
-  filter(SiteID == 9)
+  filter(SiteID == "09")
+Site10 <- raw_inverts_2 %>%
+  group_by(SiteID, genusSpecies) %>%
+  filter(SiteID == "10")
 
 Reduce(intersect, list(Site1$genusSpecies, Site2$genusSpecies, Site3$genusSpecies,
                        Site4$genusSpecies, Site5$genusSpecies, Site6$genusSpecies,
-                       Site7$genusSpecies, Site8$genusSpecies))
+                       Site7$genusSpecies, Site8$genusSpecies, Site9$genusSpecies,
+                       Site10$genusSpecies))
 
 
 ###################################################################################
@@ -171,15 +183,17 @@ Reduce(intersect, list(Site1$genusSpecies, Site2$genusSpecies, Site3$genusSpecie
 
 raw_inverts_f1 <- raw_inverts_2
 raw_inverts_f1$count <- 1
+
 # per individual
 raw_inverts_f2 <- raw_inverts_f1 %>%
-  group_by(IceCoverCat) %>%
+  group_by(IceCoverCat, SiteID) %>%
   summarise(sum(count))
 # rename column
 names(raw_inverts_f2)[names(raw_inverts_f2)=="sum(count)"] <- "totalOrganisms"
+
 # per species
 raw_inverts_f3 <- raw_inverts_f1 %>%
-  group_by(IceCoverCat, genusSpecies) %>% 
+  group_by(IceCoverCat, genusSpecies, SiteID) %>% 
   summarise(sum(count))
 # rename column
 names(raw_inverts_f3)[names(raw_inverts_f3)=="sum(count)"] <- "totalSpecies"
@@ -195,6 +209,17 @@ ggplot(data = raw_inverts_f2, aes(x = IceCoverCat, y = totalOrganisms)) +
 ggplot(data = raw_inverts_f3, aes(x = IceCoverCat, y = totalSpecies)) +
   geom_col() +
   theme_classic()
+
+# total number of organisms collected for biomarkers per site
+ggplot(data = raw_inverts_f2, aes(x = SiteID, y = totalOrganisms)) +
+  geom_col() +
+  theme_classic()
+
+# total number of species collected for biomarkers per site
+ggplot(data = raw_inverts_f3, aes(x = SiteID, y = totalSpecies)) +
+  geom_col() +
+  theme_classic()
+
 
 
 
