f04a629ced3c6f6694eb211a026ff4338e01016d
Ross Whippo <rosswhippo@gmail.com>
2021-05-05 15:27:50 -0700

    Added figure place holder scripts

--- FA_descriptive_stats.R
+++ FA_descriptive_stats.R
@@ -139,20 +139,44 @@ ggplot(filter(final_concs, stand_conc > 250), aes(x = Name, y = stand_conc, colo
   labs(x = "Fatty Acid", y = "Concentration (ng/mg)") +
   theme(axis.text.x = element_text(angle = 270, vjust = 0.1))
 
-# stacked barplot of percent contribution of each FA to total FA
-ggplot(filter(final_concs, stand_conc > 250), aes(x = FA_percent, y = Sample, fill = Name)) +
-         geom_col(position = "stack") +
-         scale_fill_viridis(discrete = TRUE)
+# stacked barplot of percent contribution of each FA to total FA (dominant)
+ggplot(filter(final_concs, stand_conc > 250 & Name != 'c19.0'), aes(x = FA_percent, y = species, fill = Name)) +
+  geom_col(position = "stack") +
+  scale_fill_viridis(discrete = TRUE) +
+  theme_classic() +
+  labs(fill = "Fatty Acid") +
+  xlab("Proportion of Total FA Content") +
+  ylab("Species")
+
+# stacked barplot of percent contribution of each FA to total FA (minimal)
+ggplot(filter(final_concs, stand_conc > 25 & stand_conc < 250 & Name != 'c19.0'), aes(x = FA_percent, y = species, fill = Name)) +
+  geom_col(position = "stack") +
+  scale_fill_viridis(discrete = TRUE) +
+  labs(fill = "Fatty Acid") +
+  xlab("Proportion of Total FA Content") +
+  ylab("Species")
+
+# stacked barplot of percent contribution of each FA to total FA (trace)
+ggplot(filter(final_concs, stand_conc < 25 & Name != 'c19.0'), aes(x = FA_percent, y = species, fill = Name)) +
+  geom_col(position = "stack") +
+  scale_fill_viridis(discrete = TRUE) +
+  labs(fill = "Fatty Acid") +
+  xlab("Proportion of Total FA Content") +
+  ylab("Species")
 
 # LIN, ALA, SDA, ARA, EPA
-ggplot(filter(final_concs, Name %in% c("c18.2n6c", "c18.3n3", "c18.4n3", "c20.4n6", "c20.5n3")), aes(x = Name, y = stand_conc, fill = Sample)) +
-         geom_col(position = "dodge") +
-         scale_fill_viridis(discrete = TRUE) +
-         scale_x_discrete(labels=c("c18.2n6c" = "LIN (c18.2n6c)", "c18.3n3" = "ALA (c18.3n3)",
-                            "c20.4n6" = "ARA (c20.4n6)", "c20.5n3" = "EPA (c20.5n3)"))
+ggplot(filter(final_concs, Name %in% c("c18.2n6c", "c18.3n3", "c18.4n3", "c20.4n6", "c20.5n3")), aes(x = Name, y = stand_conc, fill = species)) +
+  geom_col(position = "dodge") +
+  theme_classic() +
+  scale_fill_viridis(discrete = TRUE) +
+  scale_x_discrete(labels=c("c18.2n6c" = "LIN (c18.2n6c)", "c18.3n3" = "ALA (c18.3n3)",
+                            "c20.4n6" = "ARA (c20.4n6)", "c20.5n3" = "EPA (c20.5n3)")) +
+  labs(fill = "Species") +
+  xlab("Fatty Acid") +
+  ylab("Standardized Concentration (ng/ul)")
   
 
-####
+  ####
 #<<<<<<<<<<<<<<<<<<<<<<<<<<END OF SCRIPT>>>>>>>>>>>>>>>>>>>>>>>>#
 
 # SCRATCH PAD ####
