9b17718e587a4842abec52d6df19b925ea9dc6c3
Ross Whippo <rosswhippo@gmail.com>
2022-03-18 13:09:56 -0700

    Updated data with batch 3, refined figures

--- FA_descriptive_stats.R
+++ FA_descriptive_stats.R
@@ -45,6 +45,7 @@
 # 2022-02-08 Basically abandoning all previous work. Starting from scratch using
 #             Browser instead of Insight. Started w/ batch 1
 # 2022-03-07 Finished batch 1 and 2, used code to create basic summary stats
+# 2022-03-18 Finished batch 3, added to analyses
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # LOAD PACKAGES                                                                ####
@@ -74,17 +75,19 @@ library(viridis)
 # Insight_concs <- Insight_concs %>%
 #   mutate(across(where(is.character), ~na_if(., "----")))
 
-# Need to be connected to Dropbox to access
+# Need to be connected to Dropbox to access LINUX
 Whippo_FA_extraction_log <- read_csv("~/Dropbox/OSF/Fatty Acid Extractions/Whippo_FA_extraction_log.csv") #linux
+# WINDOWS
 Whippo_FA_extraction_log <- read_csv("C:/Users/rossw/Dropbox/OSF/Fatty Acid Extractions/Whippo_FA_extraction_log.csv") #windows
+
 
 # batch 1 import
 # batch1 <- read_csv("Data/Biomarkers/FattyAcids/batch1_gradients19_rawquants.csv")
   
-batch1$Conc <- batch1$Conc %>%
-  replace("-----", 0)
-  mutate(Conc_num = case_when(Conc == "-----" ~ "0")) 
-  select(c('Data Filename', 'FA', 'Ret Time', 'Conc'))
+# batch1$Conc <- batch1$Conc %>%
+#   replace("-----", 0)
+#   mutate(Conc_num = case_when(Conc == "-----" ~ "0")) 
+#   select(c('Data Filename', 'FA', 'Ret Time', 'Conc'))
   
 all_batch <- read_csv("Data/Biomarkers/FattyAcids/core_spp_final_concs.csv")
 
@@ -113,45 +116,46 @@ all_batch <- read_csv("Data/Biomarkers/FattyAcids/core_spp_final_concs.csv")
 
 
 
-############### Actual data BROWSER
+############### Actual data BROWSER NEW 2022!
 
 # create species-genus ID column (BROWSER)
-grad_conc <- all_batch %>%
-  mutate(species = case_when(startsWith(sample, "HIGR") ~ "H. grandifolius",
-                             startsWith(sample, "DEME") ~ "D. menziesii",
-                             startsWith(sample, "IRCO") ~ "I. cordata",
-                             startsWith(sample, "PHAN") ~ "P. antarctica",
-                             startsWith(sample, "PLCA") ~ "P. cartilagineum"))
+ grad_conc <- all_batch %>%
+   mutate(species = case_when(startsWith(sample, "HIGR") ~ "H. grandifolius",
+                              startsWith(sample, "DEME") ~ "D. menziesii",
+                              startsWith(sample, "IRCO") ~ "I. cordata",
+                              startsWith(sample, "PHAN") ~ "P. antarctica",
+                              startsWith(sample, "PLCA") ~ "P. cartilagineum",
+                              startsWith(sample, "MYMA") ~ "M. manginii"))
 
 
 
 # DEME only for gradient analysis
 
-deme <- grad_conc %>%
-  filter(species == "D. menziesii") %>%
-  mutate(site = str_sub(sample, 6, 7))
+ deme <- grad_conc %>%
+   filter(species == "D. menziesii") %>%
+   mutate(site = str_sub(sample, 6, 7))
 
 
-site <- c('02', '03', '04', '05', '07', '08', '09', '10', '11', '12', '13', '14', '15')
-lat <- c(-67.5567, -68.1758, -68.6921, -67.5488, -66.0894, -65.5131, -65.1043, -64.9002, -64.7932, -64.7720, -66.0251, -64.7793, -65.2402)
+ site <- c('02', '03', '04', '05', '07', '08', '09', '10', '11', '12', '13', '14', '15')
+ lat <- c(-67.5567, -68.1758, -68.6921, -67.5488, -66.0894, -65.5131, -65.1043, -64.9002, -64.7932, -64.7720, -66.0251, -64.7793, -65.2402)
 
-site_lat <- data.frame(site, lat)
+ site_lat <- data.frame(site, lat)
 
-deme_site <- left_join(deme, site_lat, by = "site")
-deme_site$lat <- as.factor(deme_site$lat)
+ deme_site <- left_join(deme, site_lat, by = "site")
+ deme_site$lat <- as.factor(deme_site$lat)
 
-ggplot(filter(deme_site, proportion > 0.1 & FA !="19:0"), aes(x = lat, y = proportion, colour = FA)) +
-  geom_point() +
-  geom_point(size = 4) +
-  theme_classic() +
-  scale_colour_viridis(discrete = TRUE, end = 0.9) +
-  labs(x = "Latitude", y = "Proportion of Total Fatty Acids") +
-  geom_line(data = deme_site %>%
-              filter(proportion > 0.1 & FA !="19:0") %>%
-              group_by(lat, FA) %>%
-              summarise("mean proportion" = mean(proportion)), 
-            aes(x = lat, y = `mean proportion`, group = FA, colour = FA), lwd = 1) +
-  coord_flip()
+ ggplot(filter(deme_site, proportion > 0.1 & FA !="19:0"), aes(x = lat, y = proportion, colour = FA)) +
+   geom_point() +
+   geom_point(size = 4) +
+    theme_classic() +
+   scale_colour_viridis(discrete = TRUE, end = 0.9) +
+   labs(x = "Latitude", y = "Proportion of Total Fatty Acids") +
+   geom_line(data = deme_site %>%
+               filter(proportion > 0.1 & FA !="19:0") %>%
+               group_by(lat, FA) %>%
+               summarise("mean proportion" = mean(proportion)), 
+             aes(x = lat, y = `mean proportion`, group = FA, colour = FA), lwd = 1) +
+   coord_flip()
 
 
 
@@ -164,13 +168,13 @@ grad_conc_wide <- grad_conc %>%
   select(FA, species, proportion, sample) %>%
   pivot_wider(names_from = FA, values_from = proportion, values_fill = 0)
 
-# select EPA, ARA, SDA, PAL, OLE, and dominant sats (16, 18)
+# select EPA, ARA, SDA, PAL, OLE, LIN, VAC, and dominant sats (16, 18)
 sub_wide <- grad_conc_wide %>%
-  select(species, sample, "16:0", "16:1w7c", "18:0", "18:1w9c", "18:4w3c", "20:4w6", "20:5w3")
-sub_wide_trans <- (1+sub_wide[,3:9])
+  select(species, sample, "14:0", "16:0", "16:1w7c", "18:0", "18:1w7c", "18:3w3", "18:4w3c", "18:1w9c", "20:4w6", "20:5w3")
+sub_wide_trans <- (sub_wide[,3:12])
 sub_wide_trans <- bind_cols(sub_wide[1:2], sub_wide_trans)
 
-batch_1_2_MDS <- metaMDS(sub_wide_trans[3:9])
+batch_1_2_MDS <- metaMDS(sub_wide_trans[3:12], autotransform = FALSE, distance = "chisq")
 batch_1_2_MDS_points <- batch_1_2_MDS$points
 batch_1_2_MDS_points <- data.frame(batch_1_2_MDS_points)
 plot_data_batch_1_2 <- data.frame(batch_1_2_MDS_points, sub_wide[,1])
@@ -181,38 +185,97 @@ chulls_tax <- ddply(plot_data_batch_1_2, .(species), function(df) df[chull(df$MD
 # DETACH PLYR so it won't mess with anything!
 detach(package:plyr)
 
-ggplot(plot_data_batch_1_2, aes(x=MDS1, y=MDS2, color = species)) +
-  theme_classic() + # optional, I just like this theme
-  geom_point(size = 4) + # set size of points to whatever you want
-  geom_polygon(data=chulls_tax, aes(x=MDS1, y=MDS2, group=species), fill=NA) + # optional 'hulls' around points
-  scale_color_viridis(discrete = TRUE, end = 0.9) # my favorite color-blind and b&w friendly palette, look at the viridis package for more details
+# create vectors to plot over MDS
+scrs <- as.data.frame(scores(batch_1_2_MDS, display = "sites"))
+scrs <- cbind(scrs, species = sub_wide_trans$species)
+
+vf <- envfit(batch_1_2_MDS, sub_wide_trans[3:12], perm = 999)
+
+spp.scrs <- as.data.frame(scores(vf, display = "vectors"))
+spp.scrs <- cbind(spp.scrs, FA = rownames(spp.scrs))
+spp.scrs <- spp.scrs %>%
+  rename(MDS1 = NMDS1,
+         MDS2 = NMDS2)
+
+ggplot(plot_data_batch_1_2, aes(x=MDS1, y=MDS2)) +
+  coord_fixed() +
+  geom_point(size = 4, aes(color = species)) + # set size of points to whatever you want
+  scale_color_viridis(discrete = TRUE, end = 0.9) + # my favorite color-blind and b&w friendly palette, look at the viridis package for more details
+  geom_segment(data = spp.scrs,
+               aes(x = 0, xend = MDS1, y = 0, yend = MDS2),
+               arrow = arrow(length = unit(0.25, "cm")), color = "grey") +
+  geom_text(data = spp.scrs, aes(label = FA), 
+            size = 3) +
+  geom_polygon(data = chulls_tax,
+               aes(x = MDS1, y = MDS2, color = species), 
+               fill = NA) + # optional 'hulls' around points
+  theme_classic()  # optional, I just like this theme
+
+
+
+
+
+
+
+
+
+
+# cluster analysis of FAs that are related
+FA_dist <- vegdist(t(sub_wide_trans[3:12]))
+FA_clust <- hclust(FA_dist, method="ward.D2") 
+
+plot(FA_clust, las = 1, 
+     main="Cluster diagram of algal fatty acids", 
+     xlab="Sample", 
+     ylab="Euclidean distance")
+
+
+# cluster analysis of Algae that are related
+Alg_dist <- vegdist(sub_wide_trans[3:12])
+Alg_clust <- hclust(Alg_dist, method="ward.D2")
+Alg_order <- data.frame(sub_wide_trans$species, Alg_clust$order)
+Alg_order <- Alg_order[order(Alg_order$Alg_clust.order), ]
+
+plot(Alg_clust, las = 1, 
+     main="Cluster diagram of algae", 
+     xlab="Sample", 
+     ylab="Euclidean distance",
+     label = Alg_order$sub_wide_trans.species)
+
+
+
 
 
 # dotplot of final concentration by species by FA
-ggplot(filter(grad_conc, proportion > 0.1 & FA !="19:0"), aes(x = FA, y = proportion, colour = species)) +
+ggplot(filter(grad_conc, FA %in% c("14:0", "16:0", "16:1w7c", "18:0", "18:1w7c", "18:3w3", "18:4w3c", "18:1w9c", "20:4w6", "20:5w3")), aes(x = FA, y = proportion, colour = species)) +
+  geom_point(size = 4, position = position_jitter(width = .1)) +
   geom_boxplot(data = grad_conc %>% 
-                 filter(proportion > 0.1 & FA !="19:0"), 
+                 filter(FA %in% c("14:0", "16:0", "16:1w7c", "18:0", "18:1w7c", "18:3w3", "18:4w3c", "18:1w9c", "20:4w6", "20:5w3")), 
                aes(group = FA, y = proportion), color = "black", alpha = 0, show.legend = FALSE) +
-  geom_point(size = 4) +
   theme_classic() +
   scale_colour_viridis(discrete = TRUE, end = 0.9) +
   labs(x = "Fatty Acid", y = "Proportion of Total Fatty Acids") +
-  scale_x_discrete(labels = c("16:0" = "PAL (16:0)", 
+  scale_x_discrete(labels = c("14:0" = "MYR (14:0)",
+                              "16:0" = "PAL (16:0)", 
                             "16:1w7c" = "PALO (16:1w7c)", 
                             "18:0" = "STE (18:0)",
+                            "18:1w7c" = "VAC (18:1w7c)",
+                            "18:3w3" = "LIN (18:3w3)",
                             "18:4w3c" = "SDA (18:4w3c)",
                             "18:1w9c" = "OLE (18:1w9c)", 
                             "20:4w6" = "ARA (20:4w6)", 
                             "20:5w3" = "EPA (20:5w3)")) +
   coord_flip()
 
+# 18:1n7, 18:3n3 < 0.1, rest = 0.1 < other FA
+
 # stacked barplot of percent contribution of each FA to total FA (dominant)
-ggplot(filter(grad_conc, proportion > 0.1 & FA != '19:0') %>%
+ggplot(filter(grad_conc, FA %in% c("14:0", "16:0", "16:1w7c", "18:0", "18:1w7c", "18:3w3", "18:4w3c", "18:1w9c", "20:4w6", "20:5w3")) %>%
          group_by(species, FA) %>%
          summarise(proportion = mean(proportion)) %>%
-         ungroup() %>%
-         mutate(species = fct_reorder(species, proportion, sum, .desc = TRUE)), aes(x = proportion, y = species, fill = FA)) +
-  geom_col(position = "stack") +
+         ungroup(),
+         aes(x = proportion, y = species, fill = factor(FA, levels = c("14:0", "18:0", "16:1w7c", "18:1w7c", "18:3w3", "18:4w3c", "18:1w9c", "16:0", "20:5w3", "20:4w6")))) +
+  geom_col(position = "stack", color = "black") +
   scale_fill_viridis(discrete = TRUE, option = "C") +
   theme_classic() +
   labs(fill = "Fatty Acid") +
@@ -230,7 +293,7 @@ ggplot(filter(grad_conc, proportion > 0.1 & FA != '19:0') %>%
 # stacked barplot of percent contribution of each FA to total FA (trace)
 ggplot(filter(grad_conc, proportion > 0.01 & proportion < 0.1 & FA != '19:0') %>%
          mutate(species = fct_reorder(species, proportion, sum, .desc = TRUE)), aes(x = proportion, y = species, fill = FA)) +
-  geom_col(position = "stack") +
+  geom_col(position = "stack", color = "black") +
   scale_fill_viridis(discrete = TRUE) +
   labs(fill = "Fatty Acid") +
   xlab("Proportion of Total FA Content") +
