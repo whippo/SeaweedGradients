70e01bf07d0431b1f5529d39dd55f4be4450d6c7
Ross Whippo <rosswhippo@gmail.com>
2022-03-25 15:51:16 -0700

    Uploaded first .csv version of all FA

--- FA_data_pipeline.R
+++ FA_data_pipeline.R
@@ -25,6 +25,11 @@
 
 # TO DO 
 
+# Sample ID definitely off for some invert samples (labeled as site 1, but 
+# definitely not), and possibly some algae samples. Need to consult the 
+# 'phonebook' to see where the errors are (appear to be mostly in sites
+# 1-3.)
+
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # TABLE OF CONTENTS                                                            ####
 #                                                                                 +
@@ -69,8 +74,18 @@ algae_step1 <- algae_raw %>%
 # join algae data with metadata by ProjID column
 algae_step2 <- algae_step1 %>%
   left_join(sample_metadata, by = "ProjID")
+# create sp.abrev and phyla columns
+algae_step3 <- algae_step2 %>%
+  mutate(sp.abrev = substr(sample, 1, 4)) %>%
+  mutate(phyla = case_when(sp.abrev == "DEME" ~ "ochrophyta",
+                           sp.abrev == "HIGR" ~ "ochrophyta",
+                           sp.abrev == "IRCO" ~ "rhodophyta",
+                           sp.abrev == "PHAN" ~ "rhodophyta",
+                           sp.abrev == "PLCA" ~ "rhodophtya",
+                           sp.abrev == "MYMA" ~ "rhodophyta"))
+
 # create list of colnames for comparison to inverts
-algae_cols <- colnames(algae_step2)
+algae_cols <- colnames(algae_step3)
 
 
 # PREPARE INVERT DATASET FOR JOINING
@@ -81,8 +96,11 @@ invert_step1 <- invert_raw %>%
 # filter out non-CORE project data
 invert_step2 <- invert_step1 %>%
   filter(str_length(ProjID) > 6)
+# join invert data with metadata by ProjID column
+invert_step3 <- invert_step2 %>%
+  left_join(sample_metadata, by = "ProjID")
 # create list of colnames for comparison to algae
-invert_cols <- colnames(invert_step2)
+invert_cols <- colnames(invert_step3)
 
 
 # find differences in column names between algae and inverts
@@ -91,9 +109,9 @@ setdiff(invert_cols, algae_cols)
 
 
 # remove "C" before colnames in inverts dataframe
-colnames(invert_step2) <- gsub(colnames(invert_step2), pattern = "C", replacement = "") 
+colnames(invert_step3) <- gsub(colnames(invert_step3), pattern = "C", replacement = "") 
 # create new list of colnames for comparison to algae
-invert_cols <- colnames(invert_step2)
+invert_cols <- colnames(invert_step3)
 # check diff after change
 setdiff(invert_cols, algae_cols)
 
@@ -101,8 +119,8 @@ shared_cols <- intersect(invert_cols, algae_cols)
 
 
 # join inverts to algae and metadata across ProjID column and coalesce columns
-joined_FA_step1 <- algae_step2 %>%
-  full_join(invert_step2, by = shared_cols) %>%
+joined_FA_step1 <- algae_step3 %>%
+  full_join(invert_step3, by = shared_cols) %>%
   mutate(sample = coalesce(sample, ID)) %>%
   mutate(IceCoverCat = coalesce(IceCoverCat, Ice.cover)) %>%
   mutate(Depth.m = coalesce(Depth.m, depth)) %>%
@@ -110,14 +128,31 @@ joined_FA_step1 <- algae_step2 %>%
   mutate(Tissue = coalesce(Tissue, tissue.type))
 # drop redundant columns and move metadata to first columns
 joined_FA_step2 <- joined_FA_step1 %>%
-  select(-c("ID", "Ice.cover", "depth", "genus", "tissue.type")) %>%
-  relocate(SiteID:sp.abrev, .after = ProjID) 
-# fill NA in FA values with 0
+  select(-c("ID", 
+            "Type",
+            "EnteredBy",
+            "Ice.cover", 
+            "depth", 
+            "genus", 
+            "tissue.type", 
+            "species.x", 
+            "FAvialNum.y",
+            "Iceoverat",
+            "species.y",
+            "omments")) %>%
+  relocate(SiteID:season, .after = ProjID) 
+# join FAvialNum columns together
 joined_FA_step3 <- joined_FA_step2 %>%
+  mutate(FAvialNum = coalesce(FAvialNum, FAvialNum.x))
+# fill NA in FA values with 0
+joined_FA_step4 <- joined_FA_step3 %>%
   mutate(across("8:0":"22:4w3", ~replace_na(., 0)))
+# drop reduntant vial number column
+joined_FA_step5 <- joined_FA_step4 %>%
+  select(-FAvialNum.x)
 
 # make sure all columns have been sorted and selected properly
-colnames(joined_FA_step3)
+colnames(joined_FA_step5)
 
 ####
 #<<<<<<<<<<<<<<<<<<<<<<<<<<END OF SCRIPT>>>>>>>>>>>>>>>>>>>>>>>>#
